d2a700b5099b931ba01c5554e459d3a6597a7073
xingengwang <xingengwang@gmail.com>
2017-02-06 21:54:21 -0600

    finish exercise7

--- exercise7/exercise7.scala
+++ exercise7/exercise7.scala
@@ -3,3 +3,6 @@
  *  11144515
  */
  
+def StreamFn(a:Stream[Any], b:Stream[Any]):Stream[Any] = a.head #:: b.head #:: StreamFn(a.tail,b.tail)
+val output = StreamFn(A,B).take(20).force
+print(output)
